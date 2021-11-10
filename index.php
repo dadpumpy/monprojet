a571c0a2cd441edad458a5538d56d6d056bfa9d3
dadpumpy <dadpumpy@gmail.com>
2021-11-10 12:15:21 +0100

    Mise en place structure base

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+include("template/head.php");
 
-echo "Bonjour et bienvenue sur mon site !";
+$phrase = "Bonjour et bienvenue"; 
+$phrase2 = "Il était une fois une personne qui voulait manger des bonbons !"; 
+
+?>
+
+		<div class="container">
+			<div class="starter-template">
+				<h1><?= $phrase ?></h1>
+				<p class="lead"><?= $phrase2 ?></p>
+			</div>
+		</div>
+<?php
+include("template/footer.php");
