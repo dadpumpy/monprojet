ac88e5ef345a70f9cc53435730a955ef8e2bf25e
benjik81 <benjamin.zanini@outlook.com>
2021-11-12 14:20:00 +0100

    Ben test

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $phrase2 = "Il était une fois une personne qui voulait manger des bonbons !";
 			<div class="starter-template">
 				<h1><?= $phrase ?></h1>
 				<p class="lead"><?= $phrase2 ?></p>
+				<a href = "https://open.spotify.com/track/7yOHHVY8AhsNt297yEcHVE?si=b6d98dcf13334bfe">
 			</div>
 		</div>
 <?php
